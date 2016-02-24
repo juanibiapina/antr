079e593588d1c543e7a430ee111bdfdb690bad3a
Juan Ibiapina <juanibiapina@gmail.com>
2016-02-24 20:55:27 -0300

    refactor: Remove comments

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,14 @@ use notify::{RecommendedWatcher, Error, Watcher};
 use std::sync::mpsc::channel;
 
 fn main() {
-    // Create a channel to receive the events.
     let (tx, rx) = channel();
 
-    // Automatically select the best implementation for your platform.
-    // You can also access each implementation directly e.g. INotifyWatcher.
     let w: Result<RecommendedWatcher, Error> = Watcher::new(tx);
 
     match w {
         Ok(mut watcher) => {
-            // Add a path to be watched. All files and directories at that path and
-            // below will be monitored for changes.
             watcher.watch(".").unwrap();
 
-            // You'll probably want to do that in a loop. The type to match for is
-            // notify::Event, look at src/lib.rs for details.
             match rx.recv() {
                 _ => println!("Recv.")
             }
