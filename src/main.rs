6b301594a648ab31d1b2c948eff0149bc63f7c41
Juan Ibiapina <juanibiapina@gmail.com>
2016-02-25 21:44:38 -0300

    feature: Only run command if there is no command already running

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,15 @@ extern crate notify;
 use std::sync::mpsc::channel;
 use std::process::Command;
 use std::env;
+use std::thread;
+use std::sync::{Arc, Mutex};
 
 use notify::{RecommendedWatcher, Watcher};
+
+struct ShellCommand {
+    command: String,
+    args: Vec<String>,
+}
 
 fn die(message: &str) -> ! {
     println!("{}", message);
@@ -12,20 +19,25 @@ fn die(message: &str) -> ! {
 }
 
 fn main() {
-    let mut args = env::args().skip(1);
+    let mut argv = env::args().skip(1);
 
-    let command = match args.next() {
+    let command = match argv.next() {
         Some(command) => command,
         None => {
             die("antr: no command passed");
         },
     };
 
-    let mut command = Command::new(command);
+    let mut args = Vec::new();
 
-    while let Some(arg) = args.next() {
-        command.arg(arg);
+    while let Some(arg) = argv.next() {
+        args.push(arg)
     }
+
+    let shell_command = Arc::new(ShellCommand {
+        command: command,
+        args: args,
+    });
 
     let (tx, rx) = channel();
 
@@ -41,10 +53,29 @@ fn main() {
         },
     }
 
+    let running = Arc::new(Mutex::new(false));
+
     while let Ok(_) = rx.recv() {
-        Command::new("clear").status().unwrap();
-        let exit_status = command.status().unwrap();
-        println!("");
-        println!("{}", exit_status);
+        let mut local_running = running.lock().unwrap();
+        if ! *local_running {
+            *local_running = true;
+            let thread_command = shell_command.clone();
+            let thread_running = running.clone();
+
+            thread::spawn(move|| {
+                Command::new("clear").status().unwrap();
+                let mut command = Command::new(&thread_command.command);
+
+                command.args(&thread_command.args);
+
+                let exit_status = command.status().unwrap();
+                println!("");
+                println!("{}", exit_status);
+
+                let mut local_running = thread_running.lock().unwrap();
+
+                *local_running = false;
+            });
+        }
     }
 }
