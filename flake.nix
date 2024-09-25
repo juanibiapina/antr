12c8b6cc5ca98bc00a8c15c920faa3bd001f54b3
Juan Ibiapina <juanibiapina@gmail.com>
2024-09-25 21:17:30 +0200

    chore: Update flake setup

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+inputs = {
+  nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  systems.url = "github:nix-systems/default";
+  devenv.url = "github:cachix/devenv";
+};
+
+nixConfig = {
+  extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+  extra-substituters = "https://devenv.cachix.org";
+};
+
+outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
+  let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+  in
+  {
+    packages = forEachSystem (system: {
+      devenv-up = self.devShells.${system}.default.config.procfileScript;
+    });
+
+    devShells = forEachSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = devenv.lib.mkShell {
+            inherit inputs pkgs;
+            modules = [
+              {
+                packages = with pkgs; [
+                ];
+
+                languages.rust.enable = true;
+              }
+            ];
+          };
+        });
+  };
+}
