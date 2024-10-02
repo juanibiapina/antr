71990602d48e8c104da2561cd166a3b2807d2e0a
Juan Ibiapina <juanibiapina@gmail.com>
2024-10-02 22:23:37 +0200

    refactor: Finish migrating errors to new system

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,6 @@
 use notify;
 use std::io;
+use std::path::PathBuf;
 use std::result;
 
 pub type Result<T> = result::Result<T, Error>;
@@ -9,4 +10,6 @@ pub enum Error {
     CantReadCurrentDirectory(std::rc::Rc<io::Error>),
     DebouncerInitializationError(std::rc::Rc<notify::Error>),
     InvalidCurrentDirectory(std::rc::Rc<io::Error>),
+    ReadEntryError(std::rc::Rc<io::Error>),
+    WatcherError(PathBuf, std::rc::Rc<notify::Error>),
 }
