781584204984dad89ad50ab34bbe4e0f761d611e
Juan Ibiapina <juanibiapina@gmail.com>
2024-10-13 11:21:11 +0200

    test: Update bats setup to new recommendations
    https://bats-core.readthedocs.io/en/stable/tutorial.html#setting-up-a-multifile-test-suite

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -1,15 +1,17 @@
-load vendor/bats-support/load
-load vendor/bats-assert/load
+common_setup() {
+  load vendor/bats-support/load
+  load vendor/bats-assert/load
 
-export ANTR_ROOT="${BATS_TEST_DIRNAME}/.."
+  export ANTR_ROOT="${BATS_TEST_DIRNAME}/.."
 
-export ANTR_TEST_DIR="${BATS_TMPDIR}/antr"
+  export ANTR_TEST_DIR="${BATS_TMPDIR}/antr"
 
-if [ -z $ANTR_BIN ]; then
-  export ANTR_BIN=$ANTR_ROOT/target/debug/antr
-fi
+  if [ -z $ANTR_BIN ]; then
+    export ANTR_BIN=$ANTR_ROOT/target/debug/antr
+  fi
 
-mkdir -p $ANTR_TEST_DIR
+  mkdir -p $ANTR_TEST_DIR
+}
 
 teardown() {
   rm -rf "$ANTR_TEST_DIR"
