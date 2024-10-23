b881b88a6ccae4556e10e7af156c8bcb339d4bda
Juan Ibiapina <juanibiapina@gmail.com>
2024-10-23 20:36:41 +0200

    feat: Add support for running command on enter key

--- tests/antr.rs
+++ tests/antr.rs
@@ -2,6 +2,7 @@ use tempfile;
 use assert_cmd::prelude::*;
 use std::process::{Command, Stdio};
 use std::time::Duration;
+use std::io::Write;
 
 #[test]
 fn file_trigger_in_subdirectory() -> Result<(), Box<dyn std::error::Error>> {
@@ -26,6 +27,39 @@ fn file_trigger_in_subdirectory() -> Result<(), Box<dyn std::error::Error>> {
 
     // create a file
     std::fs::File::create(tempdir.path().join("subdir/file"))?;
+
+    let output = child.wait_with_output().expect("child process wasn't running");
+
+    output.assert().success();
+
+    Ok(())
+}
+
+#[test]
+fn manual_trigger() -> Result<(), Box<dyn std::error::Error>> {
+    // create a temp directory
+    let tempdir = tempfile::tempdir()?;
+
+    // create a subdirectory
+    std::fs::create_dir(tempdir.path().join("subdir"))?;
+
+    let mut cmd = Command::cargo_bin("antr")?;
+    cmd.current_dir(tempdir.path());
+    cmd.arg("--runonce");
+    cmd.arg("ls");
+    cmd.stdin(Stdio::piped());
+    cmd.stdout(Stdio::piped());
+    cmd.stderr(Stdio::piped());
+    cmd.env("RUST_LOG", "info");
+
+    let mut child = cmd.spawn().expect("failed to execute process");
+
+    // Wait for the child process to start
+    std::thread::sleep(Duration::from_secs(1));
+
+    // send a newline to the child process
+    let mut child_stdin = child.stdin.take().expect("child process stdin not available");
+    writeln!(child_stdin, "")?;
 
     let output = child.wait_with_output().expect("child process wasn't running");
 
