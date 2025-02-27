741ee78c02883c38bb748a9bea8b15c37d15b180
Juan Ibiapina <juanibiapina@gmail.com>
2025-02-27 12:49:03 +0100

    dev: Remove devenv
    It has too many dependencies.

--- flake.nix
+++ flake.nix
@@ -2,15 +2,9 @@
 inputs = {
   nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   systems.url = "github:nix-systems/default";
-  devenv.url = "github:cachix/devenv";
 };
 
-nixConfig = {
-  extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-  extra-substituters = "https://devenv.cachix.org";
-};
-
-outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
+outputs = { self, nixpkgs, systems, ... } @ inputs:
   let
     forEachSystem = nixpkgs.lib.genAttrs (import systems);
   in
@@ -20,30 +14,7 @@ outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
         pkgs = nixpkgs.legacyPackages.${system};
       in
       {
-        devenv-up = self.devShells.${system}.default.config.procfileScript;
         antr = pkgs.callPackage ./default.nix { inherit pkgs; };
       });
-
-    devShells = forEachSystem
-      (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          default = devenv.lib.mkShell {
-            inherit inputs pkgs;
-            modules = [
-              {
-                packages = with pkgs; [
-                ] ++ (if pkgs.stdenv.isDarwin then [
-                  darwin.apple_sdk.frameworks.Security
-                  darwin.apple_sdk.frameworks.CoreServices
-                ] else []);
-
-                languages.rust.enable = true;
-              }
-            ];
-          };
-        });
   };
 }
